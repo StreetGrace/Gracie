cbb0a99f8702e1f62a9515a8112c80b240d83783
yue zhou <yue.zhou@bbdo.com>
2018-07-11 13:54:57 -0400

    comment out test code

--- utils_bot/ApiaiRecognizer.js
+++ utils_bot/ApiaiRecognizer.js
@@ -33,37 +33,37 @@ module.exports.recognizer = {
     }
 };
 
-function recognize (context, callback) {
-    var result = { score: 0.0, intent: null, entities: null};
-    if (context && context.message && context.message.text){
-        var msg = context.message.text;
-        var inputContexts = context.inputContexts || [];
+// function recognize (context, callback) {
+//     var result = { score: 0.0, intent: null, entities: null};
+//     if (context && context.message && context.message.text){
+//         var msg = context.message.text;
+//         var inputContexts = context.inputContexts || [];
         
-        var request = app.textRequest(msg, { 
-            sessionId: Math.random(),
-            contexts: inputContexts
-        });
-        request.on('response', function(response) {
-            var res = response.result; 
-            var intent_result = {
-                score: 1,
-                intent: res.metadata.intentName,
-                entities: res.parameters                    
-            }
+//         var request = app.textRequest(msg, { 
+//             sessionId: Math.random(),
+//             contexts: inputContexts
+//         });
+//         request.on('response', function(response) {
+//             var res = response.result; 
+//             var intent_result = {
+//                 score: 1,
+//                 intent: res.metadata.intentName,
+//                 entities: res.parameters                    
+//             }
 
-            callback(null, intent_result);
-        });
-        request.on('error', function(error) {
-            callback(error);
-        });
-        request.end();
-    }
-    else{
-        callback(null, result);
-    }
-}
+//             callback(null, intent_result);
+//         });
+//         request.on('error', function(error) {
+//             callback(error);
+//         });
+//         request.end();
+//     }
+//     else{
+//         callback(null, result);
+//     }
+// }
 
-recognize({message: {text: 'are you with police'}, inputContexts: ['police', 'universal']}, function (error, response) {
-    var intent = response.intent;
-    console.log(intent);
-})
+// recognize({message: {text: 'are you with police'}, inputContexts: ['police', 'universal']}, function (error, response) {
+//     var intent = response.intent;
+//     console.log(intent);
+// })
